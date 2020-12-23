576502fa1b1b44fbf2054112a637b2f1dea57865
keziah55 <19816994+keziah55@users.noreply.github.com>
2020-12-23 19:00:04 +0000

    adding TableWidget object

--- storyteller/dialogs.py
+++ storyteller/dialogs.py
@@ -8,9 +8,11 @@ Created on Wed Dec 23 16:32:35 2020
 
 import os
 import re
-from PyQt5.QtWidgets import (QDialog, QDialogButtonBox, QListWidget, QTableWidget,
+from PyQt5.QtWidgets import (QDialog, QDialogButtonBox, QListWidget,
                              QVBoxLayout, QAbstractItemView)
 from PyQt5.QtCore import pyqtSlot
+from .tablewidget import TableWidget
+from .editor import StoryEditor
 
 
 class OpenStoryDialog(QDialog):
@@ -25,19 +27,20 @@ class OpenStoryDialog(QDialog):
         stories = [story for story in stories if os.path.splitext(story)[1]=='.html']
         
         header = ['Title', 'Date', 'Wordcount']
-        self.storyTable = QTableWidget(len(stories), len(header))
-        self.storyTable.setHorizontalHeaderLabels(header)
+        self.storyTable = TableWidget(header)#=header, parent=self)
+        self.storyTable.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.populateTable(stories)
         
-        self.storyList = QListWidget()
-        self.storyList.addItems(stories)
-        self.storyList.setSelectionMode(QAbstractItemView.SingleSelection)
+        # self.storyList = QListWidget()
+        # self.storyList.addItems(stories)
+        # self.storyList.setSelectionMode(QAbstractItemView.SingleSelection)
         
         self.buttons = QDialogButtonBox(QDialogButtonBox.Open|QDialogButtonBox.Cancel)
         self.buttons.accepted.connect(self.accept)
         self.buttons.rejected.connect(self.close)
         
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.storyList)
+        self.layout.addWidget(self.storyTable)
         self.layout.addWidget(self.buttons)
         
         self.setLayout(self.layout)
@@ -53,8 +56,7 @@ class OpenStoryDialog(QDialog):
     @pyqtSlot()
     def accept(self):
         """ Set `story` proprety from currently selected list item. """
-        item = self.storyList.currentItem()
-        self.story = item.text()
+        self.story = self.storyTable.currentValue('title')
         super().accept()
         
     def execDialog(self):
@@ -73,6 +75,10 @@ class OpenStoryDialog(QDialog):
             srch = re.search(r"(\d{4}-\d{2}-\d{2} )(.+)", title)
             date = srch.group(1).strip()
             title = srch.group(2).strip()
-            
+            # TODO store this info in database
+            with open(os.path.join(self.path, t)) as fileobj:
+                text = fileobj.read()
+            wordcount = StoryEditor.countWordsInText(text)
+            self.storyTable.addRow(title, date, wordcount)
     
         
---
+++ storyteller/tablewidget.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 23 18:00:04 2020
+
+@author: keziah
+"""
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
+
+class TableWidget(QTableWidget):
+    """ More Pythonic version of QTableWidget.
+    
+        Parameters
+        ----------
+        header : list 
+            List of column headers. (Can provide `column` instead.)
+        columns : int
+            Number of columns. (Only required if `header` is not provided.)
+        showRowNumbers : bool
+            If True (default behaviour), row numbers will be shown.
+        parent : QObject, optional
+            Parent object
+    """
+    
+    def __init__(self, header=None, columns=None, showRowNumbers=True, parent=None):
+        if header is None and columns is None:
+            msg = "TableWidget needs either `header` or `columns` arg."
+            raise ValueError(msg)
+        
+        if header is not None and columns is not None:
+            msg = "TableWidget needs either `header` or `columns` arg, not both."
+            raise ValueError(msg)
+            
+        if columns is None:
+            columns = len(header)
+        rows = 0
+        
+        super().__init__(rows, columns, parent)
+        
+        if header is not None:
+            self.setHorizontalHeaderLabels(header)
+        else:
+            header = list(range(columns))
+        self.header = header
+        
+        self.verticalHeader().setVisible(showRowNumbers)
+        
+        
+    @property
+    def rowCount(self):
+        return super().rowCount()
+    
+    @property
+    def columnCount(self):
+        return super().columnCount()
+        
+    def addRow(self, *args):
+        """ Add row to the table.
+        
+            Supply either the values to be added, in order, or a dict, where 
+            the keys correspond to header strings.
+        """
+        if isinstance(args, (tuple, list)):
+            args = dict(zip(self.header, args))
+        elif isinstance(args[0], dict):
+            args = args[0]
+        else:
+            msg = f"Could not add item of type {type(args)} to TableWidget."
+            raise TypeError(msg)
+            
+        row = self.rowCount
+        self.insertRow(row)
+        
+        for n, key in enumerate(self.header):
+            item = QTableWidgetItem(str(args[key]))
+            self.setItem(row, n, item)
+            
+    def currentValue(self, column):
+        """ Return the value of the given column in the currently selected row. """
+        row = self.currentRow()
+        col = self.header.index(column)
+        item = self.item(row, col)
+        return item.text()
+        
